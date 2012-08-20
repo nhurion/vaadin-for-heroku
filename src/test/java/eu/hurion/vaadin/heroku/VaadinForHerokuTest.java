94f0b6e2d28a12a8e46a0a3c8b1765edf7fd6420
Nicolas Hurion <nicolas@hurion.eu>
2012-08-21 01:22:52 +0200

    Make test wait for element so it doesn't fail randomly. Add logback for tests, and bump source/target to 1.6

--- src/test/java/eu/hurion/vaadin/heroku/VaadinForHerokuTest.java
+++ src/test/java/eu/hurion/vaadin/heroku/VaadinForHerokuTest.java
@@ -1,35 +1,50 @@
 package eu.hurion.vaadin.heroku;
 
 import com.bsb.common.vaadin.embed.EmbedVaadinServer;
+import com.google.common.base.Function;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static eu.hurion.vaadin.heroku.VaadinForHeroku.localServer;
+import static org.testng.Assert.assertEquals;
 
 public class VaadinForHerokuTest {
+
     private final EmbedVaadinServer localServer = localServer(TestApplication.class).wait(false).openBrowser(false).build();
     private final WebDriver driver = new FirefoxDriver();
 
     @BeforeClass
-    public void startup(){
+    public void startup() {
         localServer.start();
     }
 
     @AfterClass
-    public void shutdown(){
+    public void shutdown() {
         localServer.stop();
         driver.close();
     }
 
     @Test(invocationCount = 5)
-    public void checkLocalDriver(){
+    public void checkLocalServer() {
         driver.get("http://localhost:8080/?restartApplication");
-        Assert.assertFalse(driver.findElements(By.id(TestApplication.TEST_LABEL_ID)).isEmpty());
+        final WebDriverWait wait = new WebDriverWait(driver, /*seconds=*/3);
+        final WebElement element = wait.until(presenceOfElementLocated(By.id(TestApplication.TEST_LABEL_ID)));
+        assertEquals(element.getText(), TestApplication.TEST_LABEL);
+    }
+
+    private Function<WebDriver, WebElement> presenceOfElementLocated(final By locator) {
+        return new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(final WebDriver driver) {
+                return driver.findElement(locator);
+            }
+        };
     }
 
 }
